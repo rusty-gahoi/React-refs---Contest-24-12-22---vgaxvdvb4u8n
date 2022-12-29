b7a7b5857e074ac6936640a819293ee3f7a999ec
rusty-gahoi <vipulrocks28@gmail.com>
2022-12-29 19:43:13 +0530

    final commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,27 @@ import '../styles/App.css';
 const App = () => {
 
 //code here 
+  const emailVal = useRef("")
+  const passValue = useRef("")
+
+const [email, setEmail] = useState("");
+const [pass, setPass] = useState("");
+
+useEffect(()=>{
+  emailVal.current = email;
+  passValue.current = pass;
+})
+
 
   return (
     <div id="main">
       Email
-      <input id="inputEmail" type="text" value={} ref={} onChange={}/><br/>
+      <input id="inputEmail" type="text" value={email}  onChange={(e)=> setEmail(e.target.value)}/><br required/>
       Password
-      <input id="inputPassword" type="text" value={} ref={} onChange={}/><br/>
-      <button id="submitButton" onClick={}>Submit</button><br/>
-      <p id="emailText">Your Email : {}</p>
-      <p id ="passwordText">Your Password : {}</p>
+      <input id="inputPassword" type="text" value={pass}  onChange={(e)=> setPass(e.target.value)}/><br required/>
+      <button id="submitButton" onClick={()=> {}}>Submit</button><br/>
+      <p id="emailText">Your Email : {emailVal.current}</p>
+      <p id ="passwordText">Your Password : {passValue.current}</p>
       
     </div>
   )
